1d6fa164a40da5e04c04010b3e2d9e7a570a00cd
Aya Hamza <aya.hamza@certigon.nl>
2022-03-22 10:42:55 +0100

    First commit

---
+++ demo/src/main/java/com/example/demo/Student/StudentClass.java
@@ -0,0 +1,10 @@
+package com.example.demo.Student;
+import java.time.LocalDate;
+
+public class StudentClass {
+    private Long Id;
+    private String Name;
+    private String Email;
+    private LocalDate Dob;
+    private Integer Age;
+}
