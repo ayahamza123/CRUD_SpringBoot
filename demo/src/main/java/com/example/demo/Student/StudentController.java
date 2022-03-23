97eef52114743519ed18036fceb3bed107feff95
Aya Hamza <aya.hamza@certigon.nl>
2022-03-23 11:28:35 +0100

    final part

--- demo/src/main/java/com/example/demo/Student/StudentController.java
+++ demo/src/main/java/com/example/demo/Student/StudentController.java
@@ -3,11 +3,13 @@ package com.example.demo.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+
 @RestController
-@RequestMapping(path="api/v1/student")
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
     private final StudentService studentService;
 
@@ -20,9 +22,22 @@ public class StudentController {
     public List<Student> GetStudents() {
         return studentService.GetStudents();
     }
+
     @PostMapping
-    public void PostStudents(@RequestBody Student student)
-    {
+    public void PostStudents(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long id) {
+        studentService.deleteStudent(id);
+    }
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required=false) String name,
+            @RequestParam(required=false) String email) {
+        studentService.updateStudent(studentId,name,email);
+    }
+
 }
